a2eed59c9d7d8d23acca9b56d512f9a9d663027c
mrchappell <michael.r.chappell@gmail.com>
2020-02-08 11:05:46 -0600

    Express assignment Week 1

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,6 +13,11 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/campsites', campsiteRouter);
+app.use('/campsites/:campsiteId', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/promotions/:promotionId', promotionRouter);
+app.use('/partners', partnerRouter);
+app.use('/partners/:partnerId', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
